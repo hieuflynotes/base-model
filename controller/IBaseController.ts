5bcb067b19fb290c6095b2b5aee4372986fa2017
kim <ktdraft0902@gmail.com>
2021-01-12 19:37:56 +0700

    update

--- controller/IBaseController.ts
+++ controller/IBaseController.ts
@@ -4,7 +4,7 @@ import { FindFilter, ListFilter, GetFilter } from "@BaseTypes/model/Filter";
 
 export interface IBaseController<T extends BaseModel> {
   find(filter: FindFilter<T>): Promise<T[]>;
-  list(filter: ListFilter<T>): Promise<Paging<T>>;
+  list(filter?: ListFilter<T>): Promise<Paging<T>>;
   get(filter: GetFilter): Promise<T | T[] | null>;
   save(t: T): Promise<T>;
   delete(id: string): Promise<T>;
