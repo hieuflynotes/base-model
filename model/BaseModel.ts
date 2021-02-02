ad1740e47d25887ed4ee56279ba9cf6a3132a95d
kim <ktdraft0902@gmail.com>
2021-02-02 13:46:15 +0700

    update

--- model/BaseModel.ts
+++ model/BaseModel.ts
@@ -5,32 +5,27 @@ import * as Yup from "yup";
 export class BaseModel {
 	id?: string = __(
 		new _Define()
-			.setSequelize({primaryKey: true,type: DataTypes.UUID,allowNull: false})
 			.setYup(Yup.string().required().uuid())
 	);
 	createdBy?: string = __(
 		new _Define()
-			.setSequelize({type: DataTypes.UUID,field: "created_by",})
 			.setYup(Yup.string().uuid())
+
 	);
 	isDeleted?: boolean = __(
 		new _Define()
-			.setSequelize({type: DataTypes.BOOLEAN,field: "is_deleted",defaultValue: false,})
 			.setYup(Yup.boolean())
 	);
 	updatedBy?: string = __(
 		new _Define()
-			.setSequelize({type: DataTypes.UUID,field: "updated_by",})
 			.setYup(Yup.string().uuid())
 	);
 	createdAt?: Date = __(
 		new _Define()
-			.setSequelize({type: DataTypes.DATE,field: "created_at",defaultValue: Sequelize.literal("NOW()")})
 			.setYup(Yup.date())
 	);
 	updatedAt?: Date = __(
 		new _Define()
-			.setSequelize({type: DataTypes.DATE,field: "updated_at",defaultValue: Sequelize.literal("NOW()"),})
 			.setYup(Yup.date())
 	);
 }
