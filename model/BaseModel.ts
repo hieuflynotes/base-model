76a6bc371af316ac70524710ce16e8550c6d2bae
lgnvpr <=>
2021-02-10 10:10:14 +0000

    update model

--- model/BaseModel.ts
+++ model/BaseModel.ts
@@ -1,21 +1,12 @@
-import {
-  data,
-  isany,
-  isoUtcDateTime,
-  isstring,
-  isuuid,
-  opt,
-  __,
-} from "@deckchair-technicians/vice";
 import { Status } from "./Status";
 
-@data
+
 export class BaseModel {
-  id?: string = __(opt(isstring()));
-  status?: Status = __(opt(isany()));
+  id?: string ;
+  status?: Status;
   createdBy ?: string;
   isDeleted ?: boolean;
-  updatedBy?: string = __(opt(isuuid()));
-  createdAt?: Date = __(opt(isoUtcDateTime()));
-  updatedAt?: Date = __(opt(isoUtcDateTime()));
+  updatedBy?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
